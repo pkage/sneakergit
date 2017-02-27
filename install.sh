33bcd120cdcdb8f3f98d2d5d036be8e2f7720eca
Patrick Kage <quadnix1@gmail.com>
2017-02-26 20:02:47 -0500

    cross platforming abs path resolution

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # symbolic link the file
 chmod +x sneakergit.py
-ln -s `realpath sneakergit.py` /usr/local/bin/sneakergit
+ln -s `readlink sneakergit.py` /usr/local/bin/sneakergit
 
 # copy the config file
 cp sneakergit.json ~/.sneakergit.json
